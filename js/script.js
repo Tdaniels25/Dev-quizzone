a5623aebc95c19dc4851c15221baef84981a6aa1
Boipelo11 <127173413+Boipelo11@users.noreply.github.com>
2023-07-10 21:32:12 +0200

    js updated

--- js/script.js
+++ js/script.js
@@ -1,164 +1,131 @@
-const quiz = [
+const quizQuestions = [
     {
         question: "Which HTML attribute is used to specify the width of an image?",
         options: [
-            "a.width",
-            "b.size",
-            "c.scale",
-            "d.length"
+            {text:"width", correct: true},
+            {text:"size", correct: false},
+            {text:"scale",correct: false},
+            {text:"length", correct: false}
         ],
-        answer: "a"
-    },
-    {
-        question: "Which CSS property is used to control the spacing between lines of text?",
-        options: [
-            "a.text-indent",
-            "b.text-transform",
-            "c.line-height",
-            "d.letter-spacing"
-        ],
-        answer: "c"
+    
     },
     {
         question: "What is the concept of inheritance in JavaScript?",
         options: [
-            "a.Inheritance is the process of creating a new object from an existing object",
-            "b.Inheritance is a way to implement multiple interfaces in JavaScript",
-            "c.Inheritance is the process of extending the functionality of a class by inheriting properties and methods from a parent class",
-            "d.Inheritance is a way to encapsulate data and behavior into a single entity in JavaScript"
+            {text:"Inheritance is the process of creating a new object from an existing object", correct: false},
+            {text:".Inheritance is a way to implement multiple interfaces in JavaScript", correct: false},
+            {text:"Inheritance is the process of extending the functionality of a class by inheriting properties and methods from a parent class",correct: true},
+            {text:"Inheritance is a way to encapsulate data and behavior into a single entity in JavaScript", correct: false}
         ],
-        answer: "c"
     },
     {
         question: "What is the purpose of the command 'git clone'?",
         options: [
-            "a.To create a new Git repository",
-            "b.To create a new branch in the current repository",
-            "c.To download a copy of a remote repository to your local machine",
-            "d.To merge changes from one branch into another branch"
+            {text:"To create a new Git repository", correct: false},
+            {text:"To create a new branch in the current repository", correct: false},
+            {text:"To download a copy of a remote repository to your local machine",correct: true},
+            {text:"To merge changes from one branch into another branch", correct: false}
         ],
-        answer: "c"
     },
     {
         question: "What is the purpose of the JavaScript method 'setTimeout()'?",
         options: [
-            "a.To pause the execution of the script for a specific time",
-            "b.To set a recurring timer for executing a function",
-            "c.To measure the time taken by a function to execute",
-            "d.To stop the execution of a JavaScript program"
+            {text:"To pause the execution of the script for a specific time", correct: true},
+            {text:"To set a recurring timer for executing a function", correct: false},
+            {text:"To measure the time taken by a function to execute",correct: false},
+            {text:"To stop the execution of a JavaScript program", correct: false}
         ],
-        answer: "a"
     },
     {
         question: "What does the term 'pull request' mean in the context of Github?",
         options: [
-            "a.A request to retrieve the latest changes from a remote repository",
-            "b.A request to merge changes from one branch to another",
-            "c.A request to revert a commit in a Git repository",
-            "d.A request to delete a repository"
+            {text:"A request to retrieve the latest changes from a remote repository", correct: true},
+            {text:"A request to merge changes from one branch to another", correct: false},
+            {text:"A request to revert a commit in a Git repository",correct: false},
+            {text:"A request to delete a repository", correct: false}
         ],
-        answer: "a"
     },
     {
         question: "What is the significance of the JavaScript keyword 'this'?",
         options: [
-            "a.It refers to the current HTML document",
-            "b.It refers to the object on which the method is invoked",
-            "c.It refers to the global object in which the function is executed",
-            "d.It refers to the parent object of the current object"
+            {text:"It refers to the current HTML document", correct: false},
+            {text:"It refers to the object on which the method is invoked", correct: true},
+            {text:"It refers to the global object in which the function is executed",correct: false},
+            {text:"It refers to the parent object of the current object", correct: false}
         ],
-        answer: "b"
     },
     {
-        question: "Which HTML tag is used to create a drop-down list in a form?",
+        question: "WWhich HTML tag is used to create a drop-down list in a form?",
         options: [
-            "a.<option>",
-            "b.<select>",
-            "c.<form>",
-            "d.<input>"
+            {text:"width", correct: true},
+            {text:"size", correct: false},
+            {text:"scale",correct: false},
+            {text:"length", correct: false}
         ],
-        answer: "b"
+    },
+    {
+        question: "Which HTML attribute is used to specify the width of an image?",
+        options: [
+            {text:"<option>", correct: false},
+            {text:"<select>", correct: true},
+            {text:"<form>",correct: false},
+            {text:"<input>", correct: false}
+        ],
     },
     {
         question: "Which type of CSS is included in a separate file with a .css extension and linked to the HTML file using the <link> tag?",
         options: [
-            "a.Internal CSS",
-            "b.Inline CSS",
-            "c.External CSS",
-            "d.Import CSS"
+            {text:"Internal CSS", correct: false},
+            {text:"Inline CSS", correct: false},
+            {text:"External CSS",correct: true},
+            {text:"Import CSS", correct: false}
         ],
-        answer: "c"
     },
     {
-        question: "What is Git used for?",
+        question: "WWhat is Git used for?",
         options: [
-            "a.To manage databases",
-            "b.To create websites",
-            "c.To design user interfaces",
-            "d.To track changes in files"
+            {text:"To manage databases", correct: false},
+            {text:"To create websites", correct: false},
+            {text:"To design user interfaces",correct: false},
+            {text:"To track changes in files", correct: true}
         ],
-        answer: "d"
     }
 ];
 
 
-// Global variables
-let currentQuestionIndex = 0;
-let score = 0;
+class Quiz {
+    constructor(questions, timer) {
+      this.questions = questions;
+      this.timer = timer;
 
-// DOM elements
-const scoreBox = document.querySelector(".scoreBox");
-const timerBox = document.querySelector(".timerBox");
-const questionBox = document.querySelector(".questionBox");
-const optionBox = document.querySelector(".optionBox");
-const nextButton = document.getElementById("nextButton");
+    }
 
-// Display question and options
-function displayQuestion() {
-  const currentQuestion = quiz[currentQuestionIndex];
-  questionBox.textContent = currentQuestion.question;
 
-  optionBox.innerHTML = "";
-  currentQuestion.options.forEach((option) => {
-    const optionElement = document.createElement("span");
-    optionElement.textContent = option;
-    optionElement.addEventListener("click", selectAnswer);
-    optionBox.appendChild(optionElement);
-  });
-}
-
-// Event listener for option selection
-function selectAnswer() {
-  const selectedOption = this.textContent;
-  const currentQuestion = quiz[currentQuestionIndex];
+    startTimeout() {
+      let totalTime = this.timer; // Timer duration in seconds
+      let minutes = 0;
+      let seconds = 0;
+      let counter = 0;
   
-  if (selectedOption === currentQuestion.answer) {
-    score++;
-  }
-
-  currentQuestionIndex++;
+      let timer = setInterval(() => {
+        counter++;
+        minutes = Math.floor((totalTime - counter) / 60);
+        seconds = totalTime - minutes * 60 - counter;
   
-  if (currentQuestionIndex < quiz.length) {
-    displayQuestion();
-  } else {
-    showResults();
-  }
-}
-
-// Event listener for "Next" button click
-nextButton.addEventListener("click", () => {
-  if (currentQuestionIndex < quiz.length) {
-    alert("Please select an option.");
-  }
-});
-
-// Function to show quiz results
-function showResults() {
-  alert(`Quiz completed. Your score is ${score} out of ${quiz.length}.`);
-  // You can perform additional actions or redirect to another page here
-}
-
-// Initialize quiz
-displayQuestion();
-
-
+        const timerBox = document.querySelector('.timerBox');
+        timerBox.textContent = `Time Left: ${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  
+        if (minutes === 0 && seconds === 0) {
+          clearInterval(timer);
+          // Timer has reached zero, handle end of quiz
+          // Call a function or perform necessary actions when the timer ends
+          // For example, show the results, submit the quiz, etc.
+        }
+      }, 1000); // Update every second
+    }
+  
+    startQuiz() {
+      this.startTimer();
+    }
+    }
+  
